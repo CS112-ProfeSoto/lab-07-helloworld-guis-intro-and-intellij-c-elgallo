b196be093c8286f3537f8f161e17a10b89ab1677
Carlos Rosas <293crosas@gmail.com>
2025-07-08 22:39:08 -0700

    Completed Lab 07 - JavaFX Hello World

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -6,12 +6,19 @@ import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
 import javafx.scene.control.Label;      //class for label component
 import javafx.scene.control.Button;     //class for button component
+import javafx.scene.control.TextField; // this is the one I imported
 import javafx.event.EventHandler;       //interface for handling events
 import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
+
+import java.sql.SQLOutput;
 
 public class HelloApplication extends Application implements EventHandler<ActionEvent>  { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
     private Button button;
+    private Label message;
+    private TextField nameField;
+    private int numClick  = 0;
+
 
     /*** DRIVER main ***/
     public static void main(String[] args) {
@@ -22,19 +29,22 @@ public class HelloApplication extends Application implements EventHandler<Action
     @Override
     public void start(Stage primaryStage) throws Exception{ //Application automatically calls this method to run (our) main javafx code. passes in primary stage (main window)
         //SETUP COMPONENTS
-        Label message = new Label("Hi, User!");
+        message = new Label("Hi, User!");
         button = new Button("Click me"); //or can set text using setText method separately
+        nameField = new TextField();
+        nameField.setPromptText("Enter your name please: ");
         button.setOnAction(this); //who the event handler is (which object/class should handle the event)
 
         //ADD COMPONENTS
         VBox layout = new VBox(); //simple layout, components are stacked on top of each other in added order
-        layout.getChildren().add(message);
-        layout.getChildren().add(button);
+        //layout.getChildren().add(message);
+        //layout.getChildren().add(button);
+        layout.getChildren().addAll(message, nameField, button);// added this rather than three individual ones
 
         //SETUP SCENE AND SHOW
         Scene scene = new Scene(layout, 300, 250); //layout, dimensions of window
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Hello World"); //setting title of main window
+        primaryStage.setTitle("Carlos Rosas"); //setting title of main window
         primaryStage.show();
     }
 
@@ -43,7 +53,15 @@ public class HelloApplication extends Application implements EventHandler<Action
     public void handle(ActionEvent actionEvent) { //generic method used to handle when events occur (like handle button click)
         //good practice to identify source, in case you have multiple event types/sources
         if(actionEvent.getSource() == button) {
-            System.out.println("Hello, JavaFX!");
+        numClick++;
+        // fetching the User's name or defaulting to user
+            String name = nameField.getText().trim();// this getText().trim() comes from the TextField class in JavaFX
+            if (name.isEmpty()) {
+                name = "USER";
+            }
+            // Updating attached label and printing to terminal
+            message.setText("Hi #" + numClick + ", " + name + "!");
+            System.out.println("Hola, CS112!");
         }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.miracosta.cs112.lab07 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens edu.miracosta.cs112.lab07 to javafx.fxml;
